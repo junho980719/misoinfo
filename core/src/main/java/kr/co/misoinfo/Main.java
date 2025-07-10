970f7c6824f187ed6120f57025c9b0240bf4740b
김준호 <wnsgh_s2@naver.com>
2025-07-11 08:34:59 +0900

    firstcommit

---
+++ core/src/main/java/kr/co/misoinfo/Main.java
@@ -0,0 +1,7 @@
+package kr.co.misoinfo;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
---
+++ wellb-api/src/test/java/kr/co/misoinfo/wellb/api/WellbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.misoinfo.wellb.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellbApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
